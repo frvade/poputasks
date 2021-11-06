b08dc11059d07ae81b8684dde55c12bfdc1a4132
frvade <1924461+frvade@users.noreply.github.com>
2021-11-06 16:47:09 +0300

    Merge pull request #4
    * init auth app
    
    * setup devise + doorkeeper
    
    * Merge remote-tracking branch 'origin/main' into week2

--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :accounts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  use_doorkeeper
+  devise_for :accounts, controllers: {
+    registrations: 'accounts/registrations',
+    sessions: 'accounts/sessions'
+  }
+
+  root to: "accounts#index"
 end
