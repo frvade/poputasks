de4a8e6132be7c7e6d74b857a836a86a9048ebbe
frvade <1924461+frvade@users.noreply.github.com>
2021-11-13 17:35:29 +0300

    fix assign

--- task_tracker/app/lib/commands/tasks/assign.rb
+++ task_tracker/app/lib/commands/tasks/assign.rb
@@ -7,11 +7,13 @@ module Commands
       param :assignee, SmartCore::Types::Protocol::InstanceOf(User)
 
       def call
-        fail!(:not_assigned) unless task.update(assignee: assignee)
+        task.assignee = assignee
+        fail!(:not_assigned) unless task.save
 
         # Business event
         event = {
           event_name: 'TaskAssigned',
+          event_version: 1,
           data: { public_id: task.public_id, assignee: { public_id: assignee.public_id } }
         }
         EventProducer.produce_sync(event, 'tasks.assigned', 'tasks-lifecycle')
